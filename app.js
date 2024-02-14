ad757a0f31b4cfe01693cac2caf3980ee35f2ebc
jubayerasif <jubaerasif101@gmail.com>
2024-02-14 12:34:15 +0600

    Working With Status Code

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get("/restaurants/:id", function (req, res) {
     }
   }
 
-  res.render("404");
+  res.status(404).render("404");
 });
 app.get("/recommend", function (req, res) {
   res.render("recommend");
@@ -67,11 +67,11 @@ app.get("/about", function (req, res) {
 });
 
 app.use(function (req, res) {
-  res.render('404');
+  res.status(404).render('404');
 });
 
 app.use(function (error, req, res, next) {
-  res.render('500');;
+  res.status(505).render('500');
 });
 
 app.listen(3000);
